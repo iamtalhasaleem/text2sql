d5be68925bed05120a20cf2636fc3b5b48fd1cb8
iamtalhasaleem <iamtalhasaleem911@gmail.com>
2024-09-18 11:04:30 +0500

    add describe tables tool

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from langchain.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder
 )
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables
 
 load_dotenv()
 chat = ChatOpenAI()
+tables = list_tables()
+# print(tables)
 
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content = 'You are an AI that has access to SQLimte database.\n{tables}'),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
--- tools/sql.py
+++ tools/sql.py
@@ -3,6 +3,12 @@ from langchain.tools import Tool
 
 conn = sqlite3.connect("db.sqlite")
 
+def list_tables():
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+    rows = c.fetchall()
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
 def run_sqlite_query(query):
     c = conn.cursor()
     try:
@@ -11,8 +17,24 @@ def run_sqlite_query(query):
     except sqlite3.OperationalError as err:
         return f"The following error occured: {str(err)}"
 
+
+
+def describe_tables(table_names):
+    c = conn.cursor()
+    tables = ', '.join("'" + table + "'" for table in table_names)
+    rows  = c.execute(f"SELECT sql FROM sqlite_master WHERE type = 'table' AND name IN ({tables});")
+    return (row[0] for row in rows if row[0] is not None)
+
+
+
 run_query_tool  = Tool.from_function(
     name = "run_sqlite_query",
     description = "Run a sqlite query",
     func = run_sqlite_query
+)
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given the list of table names, return the schema of those tables",
+    func=describe_tables
 )
